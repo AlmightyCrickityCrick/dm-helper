2f279f3d6cf76c7cb00ecf2dd6a820ff6da18196
AlmightyCrickityCrick <seong92@mail.ru>
2025-06-02 23:50:51 +0300

    fix picker input

--- app/src/main/java/com/example/dmhelper/presentation/components/input/PickerInput.kt
+++ app/src/main/java/com/example/dmhelper/presentation/components/input/PickerInput.kt
@@ -1,14 +1,18 @@
 package com.example.dmhelper.presentation.components.input
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MenuAnchorType
+import androidx.compose.material3.MenuItemColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,8 +20,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.dmhelper.R
 import com.example.dmhelper.presentation.common.PickerFormUiState
 import com.example.dmhelper.ui.theme.DMHelperTheme
 
@@ -29,7 +37,7 @@ fun PickerInput(
     placeholder: String,
     modifier: Modifier = Modifier
 ) {
-    var isExpanded by remember { mutableStateOf(false) }
+    var isExpanded by remember { mutableStateOf(true) }
     ExposedDropdownMenuBox(
         expanded = isExpanded,
         onExpandedChange = { isExpanded = !isExpanded },
@@ -42,29 +50,46 @@ fun PickerInput(
             placeholder = placeholder,
             isError = false,
             trailingIcon = {
-                ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = isExpanded,
+                    modifier = modifier.menuAnchor(MenuAnchorType.PrimaryEditable, true)
+                )
             },
             onValueChange = {},
             modifier = Modifier
                 .fillMaxWidth()
-                .clickable { isExpanded = !isExpanded } // make whole field clickable
-                .menuAnchor()
         )
         ExposedDropdownMenu(
             expanded = isExpanded,
             onDismissRequest = {
                 isExpanded = false
             },
-            modifier = Modifier.fillMaxWidth()
+            containerColor = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.width(260.dp)
         ) {
             pickerFormState.options.forEach { selectionOption ->
                 DropdownMenuItem(
-                    text = { Text(text = selectionOption.value, color = MaterialTheme.colorScheme.onSecondary) },
+                    text = {
+                        Text(
+                            text = selectionOption.value,
+                            color = MaterialTheme.colorScheme.onSecondary,
+                            modifier = modifier
+                                .fillMaxWidth()
+                                .padding(top = 12.dp, start = 12.dp)
+                                ,
+
+                            )
+                    },
                     onClick = {
                         onItemSelected(selectionOption.key)
                         isExpanded = false
                     },
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier.fillMaxWidth().height(40.dp).paint(
+                        painterResource(
+                            R.drawable.bg_wood
+                        ), contentScale = ContentScale.FillBounds
+                    ).border(3.dp, color = MaterialTheme.colorScheme.onSurface)
+
                 )
             }
         }
